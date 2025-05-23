c609950d2f973b6171d95e7e8d3fdf2a55c5d903
Ronald Bosma <ronaldbosma@outlook.com>
2025-05-23 17:46:39 +0200

    Add post and delete as possible HTTP methods and the method when calling the protected API

--- src/functionApp/FunctionApp/CallProtectedApiFunction.cs
+++ src/functionApp/FunctionApp/CallProtectedApiFunction.cs
@@ -14,29 +14,51 @@ public class CallProtectedApiFunction
     }
 
     [Function(nameof(CallProtectedApiFunction))]
-    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "delete")] HttpRequest originalRequest)
     {
         try
         {
             using var httpClient = _httpClientFactory.CreateClient("apim");
-            var request = new HttpRequestMessage(HttpMethod.Get, "/protected");
+            var method = GetHttpMethod(originalRequest.Method);
+            var request = new HttpRequestMessage(method, "/protected");
             var result = await httpClient.SendAsync(request);
 
-            return new ContentResult
-            {
-                StatusCode = (int?)result?.StatusCode,
-                Content = result?.Content is not null ? await result.Content.ReadAsStringAsync() : string.Empty,
-                ContentType = result?.Content?.Headers.ContentType?.ToString() ?? "text/plain"
-            };
+            return await CreateActionResultFromHttpResponseMessage(result);
         }
         catch (Exception ex)
         {
-            return new ContentResult
-            {
-                StatusCode = StatusCodes.Status500InternalServerError,
-                Content = ex.ToString(),
-                ContentType = "text/plain"
-            };
+            return CreateActionResultFromException(ex);
         }
+    }
+
+    private static HttpMethod GetHttpMethod(string method)
+    {
+        return method?.ToUpperInvariant() switch
+        {
+            "GET" => HttpMethod.Get,
+            "POST" => HttpMethod.Post,
+            "DELETE" => HttpMethod.Delete,
+            _ => new HttpMethod(method ?? "POST")
+        };
+    }
+
+    private static async Task<IActionResult> CreateActionResultFromHttpResponseMessage(HttpResponseMessage? result)
+    {
+        return new ContentResult
+        {
+            StatusCode = (int?)result?.StatusCode,
+            Content = result?.Content is not null ? await result.Content.ReadAsStringAsync() : string.Empty,
+            ContentType = result?.Content?.Headers.ContentType?.ToString() ?? "text/plain"
+        };
+    }
+
+    private static IActionResult CreateActionResultFromException(Exception ex)
+    {
+        return new ContentResult
+        {
+            StatusCode = StatusCodes.Status500InternalServerError,
+            Content = ex.ToString(),
+            ContentType = "text/plain"
+        };
     }
 }
