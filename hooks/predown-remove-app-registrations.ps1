5d13ddc91a61a7dd2f870871d3f219ac0158c66b
Ronald Bosma <ronaldbosma@outlook.com>
2025-10-24 14:07:15 +0200

    Remove all app registrations that have the environment specific 'azd-env-id' tag

--- hooks/predown-remove-app-registrations.ps1
+++ hooks/predown-remove-app-registrations.ps1
@@ -2,49 +2,63 @@
 # Related GitHub issue: https://github.com/Azure/azure-dev/issues/4724
 #
 # We're using a predown hook because the environment variables are (sometimes) empty in a postdown hook.
+# The Entra ID resources have a custom tag "azd-env-id: <environment-id>", so we can find and delete them.
+
+param(
+    [Parameter(Mandatory = $false)]
+    [string]$SubscriptionId = $env:AZURE_SUBSCRIPTION_ID,
+    
+    [Parameter(Mandatory = $false)]
+    [string]$AzdEnvironmentId = $env:AZD_ENVIRONMENT_ID
+)
+
+# Validate required parameters
+if ([string]::IsNullOrEmpty($SubscriptionId)) {
+    throw "SubscriptionId parameter is required. Please provide it as a parameter or set the AZURE_SUBSCRIPTION_ID environment variable."
+}
+
+if ([string]::IsNullOrEmpty($AzdEnvironmentId)) {
+    throw "AzdEnvironmentId parameter is required. Please provide it as a parameter or set the AZD_ENVIRONMENT_ID environment variable."
+}
 
 
 # First, ensure the Azure CLI is logged in and set to the correct subscription
-az account set --subscription $env:AZURE_SUBSCRIPTION_ID
+az account set --subscription $SubscriptionId
 if ($LASTEXITCODE -ne 0) {
     throw "Unable to set the Azure subscription. Please make sure that you're logged into the Azure CLI with the same credentials as the Azure Developer CLI."
 }
 
 
-function Remove-ApplicationAndServicePrincipal($uniqueName){
-    if ([string]::IsNullOrWhiteSpace($uniqueName)) {
-        Write-Host "No unique name provided. Skipping deletion of application and service principal."
-        return
-    }
+# Find all app registrations with the matching azd-env-id tag
+$targetTag = "azd-env-id: $AzdEnvironmentId"
+Write-Host "Looking for app registrations with tag: '$targetTag'"
 
-    # Get the application with the unique name
-    $app = az ad app list | ConvertFrom-Json | Where-Object { $_.uniqueName -eq $uniqueName }
+$apps = az ad app list | ConvertFrom-Json | Where-Object { $_.tags -contains $targetTag }
 
-    if ($app) {
+if ($apps) {
+    Write-Host "Found $($apps.Count) app registration(s) to delete"
+    
+    foreach ($app in $apps) {
         # Get the service principal of the application
         $sp = az ad sp list --all | ConvertFrom-Json | Where-Object { $_.appId -eq $app.appId }
         
         if ($sp) {
-            Write-Host "Deleting service principal $($sp.id) of application with unique name $uniqueName"
+            Write-Host "Deleting service principal $($sp.id) of application with unique name $($app.uniqueName)"
             # Delete the service principal (moves the service principal to the deleted items)
             az ad sp delete --id $sp.id
             # Permanently delete the service principal. If we don't do this, we can't create a new service principal with the same name.
             az rest --method DELETE --url "https://graph.microsoft.com/beta/directory/deleteditems/$($sp.id)"
         }
         else {
-            Write-Host "Unable to delete service princpal for application with unique name $uniqueName. Service principal not found."
+            Write-Host "Unable to delete service principal for application with unique name $($app.uniqueName). Service principal not found."
         }
 
-        Write-Host "Deleting application $($app.id) with unique name $uniqueName"
+        Write-Host "Deleting application $($app.id) with unique name $($app.uniqueName)"
         # Delete the application (moves the application to the deleted items)
         az ad app delete --id $app.id
         # Permanently delete the application. If we don't do this, we can't create a new application with the same name.
         az rest --method DELETE --url "https://graph.microsoft.com/beta/directory/deleteditems/$($app.id)"
-
-    } else {
-        Write-Host "Unable to delete application with unique name $uniqueName. Application not found."
     }
+} else {
+    Write-Host "No app registrations found with tag: '$targetTag'"
 }
-
-
-Remove-ApplicationAndServicePrincipal $env:ENTRA_ID_APIM_APP_REGISTRATION_NAME
