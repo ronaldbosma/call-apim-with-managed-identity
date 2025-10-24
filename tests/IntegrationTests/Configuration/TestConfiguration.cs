a88d360c8bffc6694e33c05a560b6d52aa5d92f7
Ronald Bosma <ronaldbosma@outlook.com>
2025-10-24 16:25:19 +0200

    Add tests for API Management scenario

---
+++ tests/IntegrationTests/Configuration/TestConfiguration.cs
@@ -0,0 +1,27 @@
+using IntegrationTests.Configuration.Azd;
+using Microsoft.Extensions.Configuration;
+
+namespace IntegrationTests.Configuration;
+
+/// <summary>
+/// Contains configuration settings for the integration tests.
+/// </summary>
+internal class TestConfiguration
+{
+    private static readonly Lazy<TestConfiguration> _instance = new(() =>
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .AddAzdEnvironmentVariables(optional: true) // Adds Azure Developer CLI environment variables; optional since CI/CD pipelines may use standard environment variables instead
+            .Build();
+
+        return new TestConfiguration
+        {
+            AzureApiManagementName = configuration.GetRequiredString("AZURE_API_MANAGEMENT_NAME")
+        };
+    });
+
+    public required string AzureApiManagementName { get; init; }
+
+    public static TestConfiguration Load() => _instance.Value;
+}
