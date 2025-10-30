2430ec226956d80953aa9b399c9bfc6fb84bc7f4
Ronald Bosma <ronaldbosma@outlook.com>
2025-10-30 16:05:44 +0100

    Add scope tests to repro issue

---
+++ tests/IntegrationTests/ScopeIssueTests.cs
@@ -0,0 +1,52 @@
+﻿using Azure.Core;
+using Azure.Identity;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class ScopeIssueTests
+    {
+        private const string Scope = "api://apim-managedidentity-nwe-i2jdr";
+        //private const string Scope = "https://kvaisquicksdctaqld.vault.azure.net";
+
+        [TestMethod]
+        public async Task AzdWithoutDefaultSuffix()
+        {
+            var tokenCredential = new AzureDeveloperCliCredential();
+            var tokenResult = await tokenCredential.GetTokenAsync(new TokenRequestContext([Scope]));
+
+            Assert.IsNotNull(tokenResult);
+            Assert.IsNotNull(tokenResult.Token);
+        }
+
+        [TestMethod]
+        public async Task AzWithoutDefaultSuffix()
+        {
+            var tokenCredential = new AzureCliCredential();
+            var tokenResult = await tokenCredential.GetTokenAsync(new TokenRequestContext([Scope]));
+
+            Assert.IsNotNull(tokenResult);
+            Assert.IsNotNull(tokenResult.Token);
+        }
+
+        [TestMethod]
+        public async Task AzdWithDefaultSuffix()
+        {
+            var tokenCredential = new AzureDeveloperCliCredential();
+            var tokenResult = await tokenCredential.GetTokenAsync(new TokenRequestContext([$"{Scope}/.default"]));
+
+            Assert.IsNotNull(tokenResult);
+            Assert.IsNotNull(tokenResult.Token);
+        }
+
+        [TestMethod]
+        public async Task AzWithDefaultSuffix()
+        {
+            var tokenCredential = new AzureCliCredential();
+            var tokenResult = await tokenCredential.GetTokenAsync(new TokenRequestContext([$"{Scope}/.default"]));
+
+            Assert.IsNotNull(tokenResult);
+            Assert.IsNotNull(tokenResult.Token);
+        }
+    }
+}
