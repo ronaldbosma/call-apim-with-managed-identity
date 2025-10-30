edef92fa3bd4c8954c90dbdce4cd3fc2ba0600bb
Ronald Bosma <ronaldbosma@outlook.com>
2025-10-30 14:08:45 +0100

    Split generic .env file code from azd specific code

--- tests/IntegrationTests/Configuration/Azd/AzdEnvironmentVariablesExtensions.cs
+++ tests/IntegrationTests/Configuration/Azd/AzdEnvironmentVariablesExtensions.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.FileProviders.Physical;
 
 namespace IntegrationTests.Configuration.Azd
 {
     /// <summary>
-    /// Extension methods for adding <see cref="AzdEnvironmentVariablesConfigurationProvider"/>.
+    /// Azd specific extension methods for adding <see cref="EnvFileConfigurationProvider"/>.
     /// </summary>
     internal static class AzdEnvironmentVariablesExtensions
     {
@@ -38,38 +36,18 @@ namespace IntegrationTests.Configuration.Azd
         /// <param name="path">The path to the .env file.</param>
         /// <param name="optional">Indication if loading the azd env variables is optional.</param>
         /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
-        public static IConfigurationBuilder AddAzdEnvironmentVariables(this IConfigurationBuilder builder, string path, bool optional)
+        private static IConfigurationBuilder AddAzdEnvironmentVariables(this IConfigurationBuilder builder, string path, bool optional)
         {
             if (string.IsNullOrWhiteSpace(path))
             {
                 if (!optional)
                 {
-                    throw new ArgumentException("Path to the azd .env file must be a non-empty string.", nameof(path)); 
+                    throw new ArgumentException("Path to the azd .env file must be a non-empty string.", nameof(path));
                 }
                 return builder;
-            } 
+            }
 
-            // We need to create our own PhysicalFileProvider because the default one excludes hiddens files and files starting with a dot.
-            var root = Path.GetDirectoryName(path) ?? throw new ArgumentException($"Unable to determine directory from path: {path}", nameof(path));
-            var fileProvider = new PhysicalFileProvider(root, ExclusionFilters.System);
-
-            return builder.AddAzdEnvFile(s =>
-            {
-                s.Path = Path.GetFileName(path);
-                s.Optional = optional;
-                s.ReloadOnChange = false;
-                s.FileProvider = fileProvider;
-            });
+            return builder.AddEnvFile(path, optional);
         }
-
-        /// <summary>
-        /// Add an azd environment variables configuration source to <paramref name="builder"/>.
-        /// </summary>
-        /// <param name="builder">The <see cref="IConfigurationBuilder"/> to add to.</param>
-        /// <param name="configureSource">An action to configure the source.</param>
-        /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
-        public static IConfigurationBuilder AddAzdEnvFile(this IConfigurationBuilder builder, Action<AzdEnvironmentVariablesConfigurationSource>? configureSource)
-            => builder.Add(configureSource);
-
     }
 }
---
+++ tests/IntegrationTests/Configuration/Azd/EnvFileExtensions.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders.Physical;
+
+namespace IntegrationTests.Configuration.Azd
+{
+    /// <summary>
+    /// Extension methods for adding <see cref="EnvFileConfigurationProvider"/>.
+    /// </summary>
+    internal static class EnvFileExtensions
+    {
+        /// <summary>
+        /// Adds the .env file configuration provider at <paramref name="path"/> to <paramref name="builder"/>.
+        /// </summary>
+        /// <param name="builder">The <see cref="IConfigurationBuilder"/> to add to.</param>
+        /// <param name="path">The path to the .env file.</param>
+        /// <param name="optional">Indication if loading the .env file is optional.</param>
+        /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+        public static IConfigurationBuilder AddEnvFile(this IConfigurationBuilder builder, string path, bool optional)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            if (!File.Exists(path))
+            {
+                if (!optional)
+                {
+                    throw new FileNotFoundException($"Unable to find .env file: {path}");
+                }
+                return builder;
+            }
+
+            // We need to create our own PhysicalFileProvider because the default one excludes hiddens files and files starting with a dot.
+            var root = Path.GetDirectoryName(path) ?? throw new ArgumentException($"Unable to determine directory from path: {path}", nameof(path));
+            var fileProvider = new PhysicalFileProvider(root, ExclusionFilters.System);
+
+            return builder.AddEnvFile(s =>
+            {
+                s.Path = Path.GetFileName(path);
+                s.Optional = optional;
+                s.ReloadOnChange = false;
+                s.FileProvider = fileProvider;
+            });
+        }
+
+        /// <summary>
+        /// Add the .env file configuration source to <paramref name="builder"/>.
+        /// </summary>
+        /// <param name="builder">The <see cref="IConfigurationBuilder"/> to add to.</param>
+        /// <param name="configureSource">An action to configure the source.</param>
+        /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+        public static IConfigurationBuilder AddEnvFile(this IConfigurationBuilder builder, Action<EnvFileConfigurationSource>? configureSource)
+            => builder.Add(configureSource);
+
+    }
+}
