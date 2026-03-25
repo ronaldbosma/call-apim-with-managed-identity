1566774b135132a5cf64f69d0289a1ea92b28679
Ronald Bosma <ronaldbosma@outlook.com>
2026-03-25 17:18:54 +0100

    Extract otel config into extension method

--- src/functionApp/FunctionApp/Program.cs
+++ src/functionApp/FunctionApp/Program.cs
@@ -1,15 +1,9 @@
-using Azure.Identity;
-using Azure.Monitor.OpenTelemetry.Exporter;
-
 using FunctionApp;
 
 using Microsoft.Azure.Functions.Worker.Builder;
-using Microsoft.Azure.Functions.Worker.OpenTelemetry;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
-using OpenTelemetry.Trace;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -21,22 +15,7 @@ builder.Logging.AddOpenTelemetry(logging =>
     logging.IncludeScopes = true;
 });
 
-builder.Services.AddOpenTelemetry()
-    .WithTracing(tracing => tracing
-        // Enables HttpClient instrumentation.
-        .AddHttpClientInstrumentation());
-
-builder.Services.AddOpenTelemetry().UseAzureMonitorExporter(options =>
-{
-    // Set the Azure Monitor credential to the DefaultAzureCredential.
-    // This credential will use the Azure identity of the current user or
-    // the service principal that the application is running as to authenticate
-    // to Azure Monitor.
-    options.Credential = new DefaultAzureCredential();
-});
-
-builder.Services.AddOpenTelemetry().UseFunctionsWorkerDefaults();
-
+builder.Services.ConfigureOpenTelemetry();
 builder.Services.RegisterDependencies();
 
 builder.Build().Run();
--- src/functionApp/FunctionApp/ServiceCollectionExtensions.cs
+++ src/functionApp/FunctionApp/ServiceCollectionExtensions.cs
@@ -1,13 +1,38 @@
 using Azure.Core;
 using Azure.Identity;
+using Azure.Monitor.OpenTelemetry.Exporter;
 
+using Microsoft.Azure.Functions.Worker.OpenTelemetry;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+
+using OpenTelemetry.Trace;
 
 namespace FunctionApp
 {
     internal static class ServiceCollectionExtensions
     {
+        public static IServiceCollection ConfigureOpenTelemetry(this IServiceCollection services)
+        {
+            services.AddOpenTelemetry()
+                .WithTracing(tracing => tracing
+                    // Enables HttpClient instrumentation.
+                    .AddHttpClientInstrumentation());
+
+            services.AddOpenTelemetry().UseAzureMonitorExporter(options =>
+            {
+                // Set the Azure Monitor credential to the DefaultAzureCredential.
+                // This credential will use the Azure identity of the current user or
+                // the service principal that the application is running as to authenticate
+                // to Azure Monitor.
+                options.Credential = new DefaultAzureCredential();
+            });
+
+            services.AddOpenTelemetry().UseFunctionsWorkerDefaults();
+
+            return services;
+        }
+
         public static IServiceCollection RegisterDependencies(this IServiceCollection services)
         {
             services.AddOptionsWithValidateOnStart<ApiManagementOptions>()
